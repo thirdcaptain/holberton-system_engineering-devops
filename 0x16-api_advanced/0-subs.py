edd1ff1ddcbe3d958f365d41219d0a244a6f6eba
Isaac Wong <Isaac Wong>
2018-08-30 22:10:17 +0000

    Initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+queries Reddit and returns total number of subscribers
+"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries Reddit and returns total number of subscribers
+    """
+    url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
+    headers = {'user-agent': 'thirdcaptain'}
+
+    response = requests.get(url, headers=headers).json()
+    child_list = response.get("data").get("children")
+    first_child = child_list[0].get("data")
+    subs = first_child.get("subreddit_subscribers")
+
+    return subs
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,12 @@
+# 0x16. API advanced
+
+20180830 - the purpose of this project is to further introduce to API interaction with Python
+
+### 0-subs.py
+Function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.
+
+### 1-top_ten.py
+Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+
+### 2-recurse.py
+Recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
