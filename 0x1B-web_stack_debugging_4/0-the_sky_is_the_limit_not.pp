e1537784e12cb28f74ec7cfc43943a3e1605442d
Isaac Wong <isaac@thirdcaptain.com>
2018-09-20 23:20:24 +0000

    One liner shorter

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 # Increases file descriptor limit of nginx
-$settings = '/etc/default/nginx'
-file { $settings:
-  ensure => present,
-}
 exec { 'edit file':
-  command => "sed -i 's/15/4000/g' /etc/default/nginx && sudo service nginx restart",
-  path    => [ '/bin/' ]
+  command => "/bin/sed -i 's/15/4000/g' /etc/default/nginx && sudo service nginx restart"
 }
 
